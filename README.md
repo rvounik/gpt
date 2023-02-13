a0420f09ab690fe94ebd1e59d86b2eb736488da3
Robbin van Ooij <r.vanooij@ltp.nl>
2023-02-13 12:49:55 +0100

    initial set up

---
+++ README.md
@@ -0,0 +1,8 @@
+# ChatGPT Frontend using VueJS and Vite
+
+this is based on work outlined here: https://jaykmody.com/blog/gpt-from-scratch/#gpt-architecture
+
+# To run the project:
+
+- yarn install
+- yarn run dev
---
+++ src/components/Chat.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+      response: "",
+    };
+  },
+  methods: {
+    submitItem() {
+      const inputs = encode(this.message.split(" "));
+      console.log('inputs is now:',inputs);
+
+      const output = gpt(inputs);
+      this.response = encode(output)
+    },
+  },
+};
+
+const vocab = ["all", "not", "heroes", "the", "wear", ".", "capes"];
+
+// const encodedString = encode(["the", "all"]);
+const encode = (input) => {
+  return input.map((i) => {
+    return vocab.indexOf(i) || 0;
+  });
+};
+
+// const decodedString = decode(encodedString);
+const decode = (input) => {
+  return input.map((i) => {
+    return vocab[i];
+  });
+};
+
+const argMax = (array) => {
+  return array.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+};
+
+const gpt = (inputs) => {
+
+  // const a= argMax(vocab);
+
+  return inputs;
+};
+
+</script>
+
+<template>
+  <div>
+    <h1 class="green">GPT frontend</h1>
+    <form method="POST" action="" @submit.prevent="submitItem()">
+      <input v-model="message" placeholder="say something" />
+      <button type="submit">submit</button>
+    </form>
+    {{response}}
+  </div>
+</template>
+
+<style scoped>
+.green {
+  color: blue;
+}
+h1 {
+  font-size: 2.6rem;
+}
+</style>
